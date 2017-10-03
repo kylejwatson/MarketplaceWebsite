bffba1d4c1a19f41f0d85e6674b9dcbfdb889895
kylewatson98 <kylewatson98@gmail.com>
2017-10-03 15:37:05 +0100

    emojis and stuff workshop3

--- index.php
+++ index.php
@@ -8,36 +8,48 @@
     <meta name="author" content="Kyle Watson">
     <link rel="stylesheet" type="text/css" href="style.css" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-    <script src="script.js"></script>
+    <!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="script.js"></script>-->
 </head>
 <body>
-    <main id="bio" tabindex="-1">
-        <section id="interests">
-            <p> Please enter your details below:- </p>
-            <form name="form1" method="post" action="passingdata.php">
-            <label>Date of Birth (in DD/MM/YYYY format):</label>
-            <input type="date" name="date">
-            <label>Password:</label>
-            <input type="password" name="password">
-            <label>First name:</label>
-            <input type="text" name="firstname">
-            <label>Surname:</label>
-            <input type="text" name="surname">
-            <fieldset>
-                <label>CS</label>
-                <input type="radio" name="course" value="CS">
-                <label>SE</label>
-                <input type="radio" name="course" value="SE">
-                <label>MIT</label>
-                <input type="radio" name="course" value="MIT">
-            </fieldset>
-            <label>Living on the Salford Campus</label>
-            <input type="checkbox" name="campus" value="campus">
-            <input type="submit" name="Submit" value="Submit">
-            <input type="reset" name="reset" value="Reset">
-    </form>
-        </section>
-    </main>
+<main id="bio" tabindex="-1">
+    <section id="interests">
+        <?php
+        $myArray = Array(78, 60, 62, 68, 71, 68, 73, 85, 66, 64, 76, 63, 75, 76, 73, 68, 62,
+            73, 72, 65, 74, 62, 62, 65, 64, 68, 73, 75, 79, 73);
+        $total;
+        foreach($myArray as $val){
+            $total += $val;
+        }
+
+        $cnt = count($myArray);
+        $total /= $cnt;
+        echo "<p> Average Tempurature is : $total </p>";
+        echo "<p> List of 7 lowest tempuratures : ";
+        sort($myArray);
+        for($i = 0; $i < 7; $i++){
+            echo " $myArray[$i],";
+        }
+
+        echo "</p><p> List of 7 highest tempuratures : ";
+        for($i = $cnt - 7; $i < $cnt; $i++){
+            echo " $myArray[$i],";
+        }
+        echo " </p>";
+        var_dump(($myArray));
+        $n = 500;
+        $counter = 1;
+        for($i = 0; $i <= $n; $i++){
+            for($ii = 0; $ii < $i; $ii++){
+                echo " $counter &#x1F";
+                echo $counter+100;
+                $counter++;
+            }
+            echo "<br>";
+        }
+
+        ?>
+    </section>
+</main>
 </body>
 </html>
--- passingdata.php
+++ passingdata.php
@@ -8,36 +8,67 @@
     <meta name="author" content="Kyle Watson">
     <link rel="stylesheet" type="text/css" href="style.css" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-    <script src="script.js"></script>
+    <!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>-->
+    <!--<script src="script.js"></script>-->
 </head>
 <body>
 <main id="bio" tabindex="-1">
     <section id="interests">
-	<form name="form1" method="post" action="passingdata.php">
-        <?php
-        /**
-         * Created by PhpStorm.
-         * User: Kyle
-         * Date: 26/09/2017
-         * Time: 12:30
-         */
-
-        echo "<p>Hello " . $_POST['firstname']. " ". $_POST['surname'] . ". ";
-        echo "You are studying " . $_POST['course'] . " and you are ";
-        $campus;
-        if(!isset($_POST['campus'])) {
-            echo "not ";
-        }
-        echo "living on campus.</p>";
-        $pass = password_hash($_POST['password'], PASSWORD_DEFAULT);
-        ?>
+        <form name="form1" method="post" action="passingdata.php">
             <?php
+            /**
+             * Created by PhpStorm.
+             * User: Kyle
+             * Date: 26/09/2017
+             * Time: 12:30
+             */
+
+            if(file_exists("passwords.txt")){
+                $myfile = fopen("passwords.txt", "r") or die("Unable to open file!");
+                $txt = fread($myfile,filesize("passwords.txt"));
+                if(password_verify($_POST['password'], $txt)){
+                    echo "<p> Correct password </p>";
+                }else{
+                    echo "<p> Incorrect password </p>";
+                }
+                fclose($myfile);
+            }else{
+                $pass = password_hash($_POST['password'], PASSWORD_DEFAULT);
+                $myfile = fopen("passwords.txt", "w") or die("Unable to open file!");
+                fwrite($myfile, $pass);
+                fclose($myfile);
+                echo "<p>Saved new password</p>";
+            }
+
+            echo "<p>Hello " . $_POST['firstname']. " ". $_POST['surname'] . ". ";
+            echo "You are studying " . $_POST['course'] . " and you are ";
+
+            if(!isset($_POST['campus'])) {
+                echo "not ";
+            }
+            echo "living on campus.</p>";
+
             list($dd,$mm,$yyyy) = explode("/",$_POST['date']);
             $year = intval($yyyy);
             $curYear = (int)date_create()->format("Y");
-            if(intval($dd) != 0 && intval($mm) != 0 && $year != 0){
-                echo date_create()->format("Y");
+            if(intval($dd) != 0 && intval($dd) < 32 && intval($mm) != 0 && intval($mm) < 13 && $year != 0 ){
+                $hundYear = $curYear - 100;
+                if($year > date_create()->format("Y")){
+                    echo "<p><strong> Please enter a date before this year</strong></p>";
+                    echo "<label>Date of Birth (in DD/MM/YY format):</label>
+                <input type=\"date\" name=\"date\">
+                <input type=\"submit\" name=\"Submit\" value=\"Submit\">
+                <input type=\"reset\" name=\"reset\" value=\"Reset\">";
+                }elseif($year < $hundYear){
+                    echo "<p><strong> Please enter a date after $hundYear</strong></p>";
+                    echo "<label>Date of Birth (in DD/MM/YY format):</label>
+                <input type=\"date\" name=\"date\">
+                <input type=\"submit\" name=\"Submit\" value=\"Submit\">
+                <input type=\"reset\" name=\"reset\" value=\"Reset\">";
+                }else{
+                    $thisYear = $curYear - $year;
+                    echo "<p> You are $thisYear years old</p>";
+                }
             }else{
                 echo "<p><strong> Please enter a valid date </strong></p>";
                 echo "<label>Date of Birth (in DD/MM/YY format):</label>
@@ -48,14 +79,14 @@
             ?>
 
 
-			<input type="hidden" name="password" value="<?php echo $_POST['password']; ?>">
-			<input type="hidden" name="firstname" value="<?php echo $_POST['firstname']; ?>">
-			<input type="hidden" name="surname" value="<?php echo $_POST['surname']; ?>">
-			<input type="hidden" name="course" value="<?php echo $_POST['course']; ?>">
-			<input type="hidden" name="campus" value="<?php if(isset($_POST['campus'])) {
-                                                    echo "campus";
-                                                    } ?>">
-	</form>
+            <input type="hidden" name="password" value="<?php echo $_POST['password']; ?>">
+            <input type="hidden" name="firstname" value="<?php echo $_POST['firstname']; ?>">
+            <input type="hidden" name="surname" value="<?php echo $_POST['surname']; ?>">
+            <input type="hidden" name="course" value="<?php echo $_POST['course']; ?>">
+            <input type="hidden" name="campus" value="<?php if(isset($_POST['campus'])) {
+                echo "campus";
+            } ?>">
+        </form>
     </section>
 </main>
 </body>

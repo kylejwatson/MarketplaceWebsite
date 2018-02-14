95bff7ff2c8c1775a13e212b750b1bb97b626fd8
Kyle Watson <kylewatson98@gmail.com>
2018-02-14 23:23:42 +0000

    added a bit of styling and validation

--- index.php
+++ index.php
@@ -1,8 +1,34 @@
 <?php
+
 $view = new stdClass();
-$view->pageTitle = 'Homepage';
 if(session_status() == 1)
     session_start();
 if(isset($_SESSION["user"]))
     $view->user = $_SESSION["user"];
-require_once('Views/index.phtml');
+
+$view->pageTitle = 'Adverts';
+
+require_once('Models/Advert.php');
+require_once('Models/DBConnection.php');
+$server = new DBConnection();
+$conn = $server->connect();
+$ad = new Advert('');
+$adSuccess = $ad->getAds($conn);
+if(is_string($adSuccess)) {
+    $view->status = $adSuccess;
+}else{
+    $view->ads = $adSuccess;
+    $view->img = array();
+    foreach($view->ads as $ad){
+        $imgPath = "images/adverts/$ad[0]_0.*";
+        $result = glob($imgPath);
+        if(count($result)>0)
+            array_push($view->img, glob($imgPath)[0]);
+        else
+            array_push($view->img,"images/adverts/default.png");
+    }
+    $view->total = count($view->ads);
+}
+
+//show edit user page
+require_once('Views/adlist.phtml');
